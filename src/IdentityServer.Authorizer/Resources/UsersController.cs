e38c5f26e0381cd94b202de510f6dd92c99722a0
Ítalo Silveira <italobrian@gmail.com>
2019-01-30 15:54:16 -0300

    Project created

---
+++ src/IdentityServer.Authorizer/Resources/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Authorizer.Resources
+{
+    [Route("api/v1/[controller]")]
+    public class UsersController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> Get()
+        {
+            return new List<string> { "user1", "user2", "user3" };
+        }
+    }
+}
