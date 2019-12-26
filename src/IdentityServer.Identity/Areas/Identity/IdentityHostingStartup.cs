a4c7160d0969927ec20e0c502abbd9fb42ec5d22
Ítalo Silveira <italobrian@gmail.com>
2019-12-26 15:33:30 -0300

    Scaffold identity

---
+++ src/IdentityServer.Identity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using IdentityServer.Authorizer.Core.Infrastructures.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(IdentityServer.Identity.Areas.Identity.IdentityHostingStartup))]
+namespace IdentityServer.Identity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
